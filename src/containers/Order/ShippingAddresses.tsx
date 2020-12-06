8cd71ed122db807680a867c091ae16167fdbfb5d
ashish8796 <ashishsaini.ak@gmail.com>
2020-12-06 21:48:33 +0530

    Now working on to show all the order

--- src/containers/Order/ShippingAddresses.tsx
+++ src/containers/Order/ShippingAddresses.tsx
@@ -29,27 +29,6 @@ function ShippingAddresses() {
     getShippingResionById();
   }, []);
 
-  // const shippingOption = [
-  //   {
-  //     shipping_cost: "20.00",
-  //     shipping_id: 1,
-  //     shipping_region_id: 2,
-  //     shipping_type: "Next Day Delivery ($20)",
-  //   },
-  //   {
-  //     shipping_id: 2,
-  //     shipping_type: "3-4 Days ($10)",
-  //     shipping_cost: "10.00",
-  //     shipping_region_id: 2,
-  //   },
-  //   {
-  //     shipping_id: 3,
-  //     shipping_type: "7 Days ($5)",
-  //     shipping_cost: "5.00",
-  //     shipping_region_id: 2,
-  //   },
-  // ];
-
   const handleOnChange = (event: any) => {
     setSelectedOption({ value: event.target.value, id: event.target.id });
   };
